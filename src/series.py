1a4776d62505b4f2c931d9911b74ca99f1dc6315
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-06 14:40:48 -0800

    adding sum_series function

--- src/series.py
+++ src/series.py
@@ -19,8 +19,21 @@ def lucas(n):
     for i in range(n):
         if i == 0:
             series.append(2)
-        if i == 1:
+        elif i == 1:
             series.append(1)
+        else:
+            series.append(series[i - 2] + series[i - 1])
+    return series[-1]
+
+
+def sum_series(n, m=0, o=1):
+    """Return a fibonacci sequence starting at n"""
+    series = []
+    for i in range(n):
+        if i == 0:
+            series.append(m)
+        elif i == 1:
+            series.append(o)
         else:
             series.append(series[i - 2] + series[i - 1])
     return series[-1]
--- src/test_series.py
+++ src/test_series.py
@@ -3,7 +3,7 @@
 import pytest
 
 
-PARAMS_TABLE_FIBONACCHI = [
+PARAMS_TABLE_FIBONACCI = [
     [1, 0],
     [2, 1],
     [3, 1],
@@ -15,11 +15,35 @@ PARAMS_TABLE_FIBONACCHI = [
 ]
 
 
-PARAMS_TABLE_LUCAS = []
+PARAMS_TABLE_LUCAS = [
+    [1, 2],
+    [2, 1],
+    [3, 3],
+    [4, 4],
+    [5, 7],
+    [6, 11],
+    [7, 18],
+    [8, 29],
+]
 
 
-@pytest.mark.parametrize("n, result", PARAMS_TABLE_FIBONACCHI)
-def test_fibonacchi(n, result):
-    """M and N should add up to result to create a fibbonachi sequence."""
+PARAMS_TABLE_SUM = []
+
+
+@pytest.mark.parametrize("n, result", PARAMS_TABLE_FIBONACCI)
+def test_fibonacci(n, result):
+    """Running function fibonacci(n) should equal result."""
     from series import fibonacci
     assert fibonacci(n) == result
+
+
+@pytest.mark.parametrize("n, result", PARAMS_TABLE_LUCAS)
+def test_lucas(n, result):
+    """Running function lucas(n) should equal result."""
+    from series import lucas
+    assert lucas(n) == result
+
+
+@pytest.mark.parametrize("n, m, o, result", PARAMS_TABLE_SUM)
+def test_sum(n, m, o, result):
+    """Running function returns fibonacci sequence starting at n"""
